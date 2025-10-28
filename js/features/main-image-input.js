822a3e1cf9ea556581653e0cb9ed589bfc1ec696
kasterra <kasterra@naver.com>
2025-10-28 11:52:57 +0900

    feat : main-image-input

--- js/features/main-image-input.js
+++ js/features/main-image-input.js
@@ -2,4 +2,28 @@
  * main-image-input 컴포넌트를 위한 기능 모듈 JS 입니다.
  * @param {HTMLElement} root
  */
-function initMainImageInput(root) {}
+export function initMainImageInput(root) {
+  if (!(root instanceof HTMLElement)) return;
+  /** @type {HTMLInputElement|null} */
+  const input = root.querySelector('input[type="file"]');
+  const img = /** @type {HTMLImageElement} */ (root.querySelector("img"));
+  if (!input || !img) return;
+
+  img.addEventListener("click", () => input.click());
+
+  function handleInputChange() {
+    const picked = input.files?.[0];
+    if (!picked) return;
+    const url = URL.createObjectURL(picked);
+    img.src = url;
+    console.log(url);
+    img.onload = () => URL.revokeObjectURL(url);
+    img.hidden = false;
+    img.ariaHidden = false;
+    const placeholder = root.querySelector(".main-image-input__placeholder");
+    placeholder.hidden = true;
+    placeholder.ariaHidden = true;
+  }
+
+  input.addEventListener("change", handleInputChange);
+}
--- js/pages/index.js
+++ js/pages/index.js
@@ -1,3 +1,5 @@
+import { initMainImageInput } from "../features/main-image-input.js";
 import { initSubImageInput } from "../features/sub-image-input.js";
 
+initMainImageInput(document.querySelector(".main-image-input"));
 initSubImageInput(document.querySelector(".sub-image-input"));
